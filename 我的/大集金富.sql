e4d74eb5bbc2debc31dbe69656b8efea653e8b39
mmstg0117 <540237773@qq.com>
2017-10-17 10:06:50 +0800

    gengxin

---
+++ 我的/大集金富.sql
@@ -0,0 +1,48 @@
+
+
+-- *******************借款录入借款录入借款录入借款录入借款录入借款录入借款录入*****************-----
+SELECT * from p_project;
+
+
+-- *******************投资投资投资投资投资投资投资投资投资投资投资投资投资投资*****************-----
+SELECT * from p_project;
+select * from p_project_order;
+select * from a_wallet;
+select * from a_wallet_record;
+select * from m_marketing_record；
+select * from c_user_coupon;
+
+
+-- ************上架上架上架上架上架上架上架上架上架上架上架上架上架上上架************-----
+
+select * from p_project;
+
+-- ************定标定标定标定标定标定标定标定标定标定标定标定标************-----
+
+select * from p_project;
+
+-- ************流标流标流标流标流标流标流标流标流标流标流标流标流标************-----
+
+select * from p_project;
+select * from p_project_order;
+select * from a_wallet;
+select * from a_wallet_record;
+select * from m_marketing_record；
+select * from c_user_coupon;
+
+-- 放款放款放款放款放款放款放款放款放款放款放款放款放款放款放款放款放款------------------------
+select * from p_project;
+select * from p_project_order_payment_plan；
+select * from p_project_order;
+select * from a_wallet;
+select * from a_wallet_record;
+
+-- 回款回款回款回款回款回款回款回款回款回款回款回款回款回款&&&&兑付兑付兑付兑付兑付兑付兑付兑付兑付----------------------------
+
+select * from p_project;
+select * from p_project_order_payment_plan；
+select * from p_project_order;
+select * from a_wallet;
+select * from a_wallet_record;
+select * from  p_project_repayment_plan；
+
